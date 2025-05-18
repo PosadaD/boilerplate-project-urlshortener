24df646ededff19b32a95792db93aac029a24ebd
DIEGO POSADA <dapiposada@gmail.com>
2025-05-18 16:59:22 -0600

    feat: se agregaron 2 endpoint donde se manda la informacion de un input desde el html con metodo post y se almacena en la base de datos, asi como un endpoint para obtener la url y redireccionar al la pagina de referencia

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const dns = require('dns');
+
+mongoose.connect(process.env.MONGO_URI)
+
+
+const urlSchema = new mongoose.Schema({
+  original_url:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  short_url:{
+    type: Number,
+    required: true
+  }
+})
+
+
+const URLs = mongoose.model("url", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,14 +30,87 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+
+
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.route('/api/shorturl')
+.post ((req, res)=>{
+
+  req.url = req.body.url
+
+  const parsedUrl = new URL(req.url);
+  const hostname = parsedUrl.hostname;
+
+  dns.lookup(hostname, async(err) => {
+
+    if (err && err.code === 'ENOTFOUND') {
+      res.json({ error: 'invalid url' });
+    }else{
+      
+      try {
+        const savedURL = await URLs.findOne({ original_url: req.url}).select("short_url");
+
+        if(savedURL){
+          req.short = savedURL.short_url
+        }else{
+          const ultimaURL = await URLs.findOne().sort({ _id: -1 }).limit(1).select("short_url");
+          if(ultimaURL === null) req.short = 1;
+          else{
+            req.short = ultimaURL.short_url + 1;
+          }
+
+          let newURL = new URLs({
+            original_url: `${req.url}`,
+            short_url: req.short
+          })
+
+          
+          try{
+            newURL.save() 
+            console.log("Nueva URLs guardada")
+          }catch(error){
+            if(error) return console.log(error)
+          }        
+        }
+
+      } catch (err) {
+        console.error(err);
+      }
+      
+      res.json({ 
+        original_url: `${req.url}`,
+        short_url: req.short
+      });
+    }
+  });
+
+})
+
+app.get("/api/shorturl/:data", async (req, res)=>{
+  
+  const shortUrl = parseInt(req.params.data);
+  const searchUrl = await URLs.findOne({short_url: shortUrl})
+  if(searchUrl){
+    res.redirect(searchUrl.original_url)
+  }else{
+    res.json({
+      error:	"No short URL found for the given input"
+    })
+  }
+})
+
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
